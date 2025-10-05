a64e1e3aeb8a91d7f1bf462cf29d8d2553448828
Hermes Trigs <allenroger451@gmail.com>
2025-10-05 19:10:14 +0100

    feat(forms): add create, update, and delete functionality for posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DeleteView,UpdateView, ListView, DetailView
+from posts.models import Post, Author
+from django.urls import reverse_lazy
+from .forms import PostForm
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(published=True).order_by("-published_at")
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+    context_object_name = "posts"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/post_form.html"
+    success_url = reverse_lazy("post_list")
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        # temporary: assign first author
+        post.author = Author.objects.first()
+        post.save()
+        return super().form_valid(form)
+
+    
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+    success_url = reverse_lazy("post_list")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "posts/post_confirm_delete.html"
+    successful_url = reverse_lazy('post_list')
+
+        
