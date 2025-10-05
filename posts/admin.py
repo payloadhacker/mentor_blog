cb43bc6cb892a40de44ece3831a3b3a60b8e187d
Hermes Trigs <allenroger451@gmail.com>
2025-10-05 15:50:28 +0100

    feat(admin): register Post, Author, Tag with custom ModelAdmin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from posts.models import Author, Post, Tag
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', "bio")
+    search_fields = ('user_username',)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ( 'name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display  = ( 'title', 'author', 'published', 'published_at')
+    list_filter = ('published', 'author',)
+    search_fields = ('title', 'body', 'author__user__username')
+    prepopulated_fields = {'slug': ('title',)}
