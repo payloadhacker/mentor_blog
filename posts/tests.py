df383123164e6edfd168267394fb9bcf971a04d5
Hermes Trigs <allenroger451@gmail.com>
2025-10-05 14:56:13 +0100

    git checkout -b feature/models git push -u origin feature/models

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from posts.models import Author, Post, Tag
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+    def test_create_post_with_author_and_tag(self):
+        user = User.objects.create_user(username="alice", password= 'password123')
+        author = Author.objects.create(user=user, bio="A passionate writer.")
+        tag = Tag.objects.create(name="django")
+        post = Post.objects.create(
+            title="My First Post",
+            slug="my-first-post",
+            body="Hello world!",
+            author=author,
+            published=True
+        )
+        post.tags.add(tag)
+        self.assertEqual(post.author.user.username, "alice")
+        self.assertIn(tag, post.tags.all())
